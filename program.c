4cec7e0ad13226f9b1d819b9dfa1568d975d0c37
Jimmacle <mindstorms11@cox.net>
2015-02-27 10:27:38 -0800

    First period changes
    Changed "floor" to "flr" in addToQueue, added a LED changing function

--- program.c
+++ program.c
@@ -9,27 +9,28 @@
 #pragma config(Sensor, dgtl8,  led3,           sensorLEDtoVCC)
 #pragma config(Motor,  port2,           liftMotor,     tmotorVex393_MC29, openLoop)
 
-int currentFloor;
+int currentFloor = 1;
 int queue[3];
 bool vbtn1;
 bool vbtn2;
 bool vbtn3;
 
+//QUEUE FUNCTIONS AND PROCESS
 bool queueContains(int n)
 {
 	for (int i = 0; i < sizeof(queue)/sizeof(int); i++) if (queue[i]==n) return true;
 	return false;
 }
 
-bool addToQueue(int floor)
+bool addToQueue(int flr)
 {
-    if (currentFloor != floor && !queueContains(floor)) //if the elevator isn't already on that floor or queued to go to that floor
-    {
+	if (currentFloor != flr && !queueContains(flr)) //if the elevator isn't already on that floor or queued to go to that floor
+	{
 		for (int i = 0; i < sizeof(queue)/sizeof(int); i++) //check each value in the queue
 		{
 			if (queue[i]==0)
 			{
-				queue[i]=floor; //add the floor to the first "empty" location in the array
+				queue[i]=flr; //add the floor to the first "empty" location in the array
 				return true; //don't check any other locations, return that the floor was added
 			}
 		}
@@ -37,6 +38,26 @@ bool addToQueue(int floor)
 	return false; //return that the floor wasn't added
 }
 
+void changeLEDs()
+{
+	SensorValue[led1] = 0;
+	SensorValue[led2] = 0;
+	SensorValue[led3] = 0;
+	switch (currentFloor)
+	{
+	case 1:
+		SensorValue[led1] = 1;
+		break;
+
+	case 2:
+		SensorValue[led2] = 1;
+		break;
+
+	case 3:
+		SensorValue[led3] = 1;
+		break;
+	}
+}
 task queueManager()
 {
 	while(1) //poll the states of the 3 buttons, and if one is pressed add its floor to the queue
@@ -66,11 +87,13 @@ task queueManager()
 	}
 }
 
+//MAIN TASK AND PROCESS
 task main()
 {
 	startTask(queueManager);
 	while(1) //add elevator control code here
-    {
+	{
+		changeLEDs();
 
-    }
+	}
 }
